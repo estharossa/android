a5a7873d35c79169b36e9457a3aff0ab55fa4647
estharossa <danik.encore@gmail.com>
2020-05-02 11:54:39 +0600

    [ADDED] feedback btn

--- translator/app/src/main/java/com/android/translator/view/AboutFragment.kt
+++ translator/app/src/main/java/com/android/translator/view/AboutFragment.kt
@@ -1,11 +1,15 @@
 package com.android.translator.view
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import com.android.translator.R
 import kotlinx.android.synthetic.main.fragment_about.*
 
@@ -33,6 +37,9 @@ class AboutFragment : Fragment() {
         shareButton.setOnClickListener {
             shareApp()
         }
+        feedbackButton.setOnClickListener {
+            sendFeedback()
+        }
     }
 
     private fun shareApp(){
@@ -42,5 +49,31 @@ class AboutFragment : Fragment() {
         intent.putExtra(Intent.EXTRA_TEXT, appLink)
         intent.type = "text/plain"
         startActivity(Intent.createChooser(intent, "ПОДЕЛИТЬСЯ ЧЕРЕЗ"))
+    }
+
+    private fun sendFeedback() {
+        var message = ""
+        val type = InputType.TYPE_CLASS_TEXT or
+                InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        val dialog: MaterialDialog = MaterialDialog(context!!).show {
+            input(
+                hint = "Напишите нам о приложении...",
+                inputType = type
+            ) { materialDialog: MaterialDialog, charSequence: CharSequence ->
+                charSequence.forEach {
+                    message += it
+                }
+            }
+            positiveButton(R.string.submit) {
+                val mIntent = Intent(Intent.ACTION_SEND)
+                mIntent.data = Uri.parse("mailto:")
+                mIntent.type = "text/plain"
+                mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("danik.encore@gmail.com"))
+                mIntent.putExtra(Intent.EXTRA_SUBJECT, "Easy Translator")
+                mIntent.putExtra(Intent.EXTRA_TEXT, message)
+                startActivity(Intent.createChooser(mIntent, "Выберите почтовый клиент"))
+            }
+
+        }
     }
 }
